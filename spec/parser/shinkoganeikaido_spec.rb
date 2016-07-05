b7220cdd1c7c7e541093e1e33b5d95b1b3ac9c49
中條 剛 <tsuyoshi-chujo@navitime.co.jp>
2016-07-05 09:00:46 +0900

    [add] test for parsing pc site

--- spec/parser/shinkoganeikaido_spec.rb
+++ spec/parser/shinkoganeikaido_spec.rb
@@ -4,9 +4,14 @@ $:.unshift(File.dirname(File.expand_path(__FILE__)) + '/../../data')
 require "spec_helper"
 require "shinkoganeikaido"
 
-describe "Shinkoganeikaido" do
-  it "is named 'jiro'" do
+describe "新小金井街道店" do
+  it "メルマガから取得した2016年7月の休業日が4, 11, 17, 18, 25になること" do
     parser = ShinkoganeikaidoParser.new
     expect(parser.parse).to eq ["4","11","17","18","25"]
+  end
+
+  it "PC店から取得した2016年7月の休業日が4, 11, 18, 25になること" do
+    parser = ShinkoganeikaidoParser.new
+    expect(parser.parse_pc_site).to eq ["4","11","18","25"]
   end
 end
