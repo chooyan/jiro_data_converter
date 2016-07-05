d5aba87acba6381783ba073b931a5341b193e6ed
中條 剛 <tsuyoshi-chujo@navitime.co.jp>
2016-07-05 15:00:11 +0900

    [add] logic to get holiday list

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 $:.unshift(File.dirname(File.expand_path(__FILE__)) + '/parser')
 $:.unshift(File.dirname(File.expand_path(__FILE__)) + '/data')
+$:.unshift(File.dirname(File.expand_path(__FILE__)) + '/util')
 
 require "shinkoganeikaido"
+require "holiday"
 
-puts ShinkoganeikaidoParser.new.parse
+# puts ShinkoganeikaidoParser.new.parse
+Holiday::get(2016)
--- util/holiday.rb
+++ util/holiday.rb
@@ -1,5 +1,16 @@
+require "open-uri"
+
 module Holiday
-  def get(year)
-    
+  def self.get(year)
+    line_num = 0
+    holidays = []
+    open("http://calendar-service.net/cal?start_year=#{year}&start_mon=1&end_year=#{year}&end_mon=12&year_style=normal&month_style=ja&wday_style=ja&format=csv&holiday_only=1&zero_padding=1") { |f| 
+      f.each_line { |line| 
+        data = line.split(",")
+        next unless data[1].match(/[0-9]{2}/)
+        holidays.push("#{data[1]}#{data[2]}")
+      }
+    }
+    holidays
   end
 end
